a664da8ae9ab714b4ba1fef7fe179b9bc9c37510
Harsh Adarsh <108008827+harshadarsh2001@users.noreply.github.com>
2024-02-02 14:39:48 +0530

    Update ApiError.java

--- src/main/java/com/harshproject/exception/ApiError.java
+++ src/main/java/com/harshproject/exception/ApiError.java
@@ -1,11 +1,7 @@
 package com.harshproject.exception;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,11 +14,4 @@ public class ApiError {
     private HttpStatus status;
     private String message;
     private LocalDateTime timestamp;
-    private Map<String, String> errors = new HashMap<>();
-
-    // Getter and Setter methods for errors
-
-    public void addError(String key, String value) {
-        errors.put(key, value);
-    }
 }
